d616c2ce65366066774cdf103ac955bbcab9a4b8
Alun Turner <alun.turner@googlemail.com>
2020-09-02 10:35:48 +0100

    complete m5

--- app/app.ts
+++ app/app.ts
@@ -1,49 +1,21 @@
 /// <reference path="player.ts" />
+/// <reference path="game.ts" />
 
-function startGame() {
-  let playerName: string | undefined = getInputValue("playername");
-  logPlayer(playerName);
+let newGame: Game;
 
-  postScore(200, playerName);
-  postScore(-5, playerName);
-}
+// handler for start game
+document.getElementById("startGame")!.addEventListener("click", () => {
+  const player: Player = new Player();
+  player.name = Utility.getInputValue("playername");
 
-function logPlayer(name: string = "MultiMath player"): void {
-  console.log(`New game starting for player: ${name}`);
-}
+  const problemCount: number = Number(Utility.getInputValue("problemCount"));
+  const factor: number = Number(Utility.getInputValue("factor"));
 
-function getInputValue(elementID: string): string | undefined {
-  const inputElement: HTMLInputElement = <HTMLInputElement>(
-    document.getElementById(elementID)
-  );
+  newGame = new Game(player, problemCount, factor);
+  newGame.displayGame();
+});
 
-  if (inputElement.value === "") return undefined;
-  return inputElement.value;
-}
-
-function postScore(
-  score: number,
-  playerName: string = "MultiMath Player"
-): void {
-  let logger: (value: string) => void;
-  if (score < 0) logger = logError;
-  else logger = logMessage;
-  const scoreElement: HTMLElement | null = document.getElementById(
-    "postedScores"
-  );
-  // ....! is the ts non-null assertion operator
-  scoreElement!.innerText = `${score} - ${playerName}`;
-  logger(`Score: ${score}`);
-}
-function logError(err: string): void {
-  console.error(err);
-}
-function logMessage(message: string): void {
-  console.log(message);
-}
-
-document.getElementById("startGame")!.addEventListener("click", startGame);
-
-let firstPlayer: Player = new Player();
-firstPlayer.name = "Tony Soprano";
-console.log(firstPlayer.formatName());
+// handler for calculate score button
+document.getElementById("calculate")!.addEventListener("click", () => {
+  newGame.calculateScore();
+});
---
+++ app/game.ts
@@ -0,0 +1,62 @@
+/// <reference path="utility.ts" />
+/// <reference path="result.ts" />
+/// <reference path="player.ts" />
+/// <reference path="scoreboard.ts" />
+
+class Game {
+  private scoreboard: Scoreboard = new Scoreboard();
+
+  constructor(
+    public player: Player,
+    public problemCount: number,
+    public factor: number
+  ) {}
+
+  displayGame(): void {
+    // create the game HTML
+    let gameForm: string = "";
+    for (let i = 1; i <= this.problemCount; i++) {
+      gameForm += '<div class="form-group">';
+      gameForm +=
+        '<label for="answer' + i + '" class="col-sm-2 control-label">';
+      gameForm += String(this.factor) + " x " + i + " = </label>";
+      gameForm +=
+        '<div class="col-sm-1"><input type="text" class="form-control" id="answer' +
+        i +
+        '" size="5" /></div>';
+      gameForm += "</div>";
+    }
+
+    // add element to game page
+    const gameElement: HTMLElement = document.getElementById("game")!;
+    gameElement.innerHTML = gameForm;
+
+    // activate the button
+    document.getElementById("calculate")!.removeAttribute("disabled");
+  }
+
+  calculateScore(): void {
+    let score: number = 0;
+
+    // calculate the number of correct answers
+    for (let i = 1; i <= this.problemCount; i++) {
+      const answer: number = Number(Utility.getInputValue("answer" + i));
+      if (i * this.factor === answer) score++;
+    }
+
+    // create a new Result object for the Scoreboard
+    const result: Result = {
+      playerName: this.player.name,
+      score: score,
+      problemCount: this.problemCount,
+      factor: this.factor,
+    };
+
+    // add result and update scoreboard
+    this.scoreboard.addResult(result);
+    this.scoreboard.updateScoreboard();
+
+    // disable the button
+    document.getElementById("calculate")!.setAttribute("disabled", "true");
+  }
+}
